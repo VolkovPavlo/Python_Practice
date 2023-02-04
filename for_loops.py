660c276c54c0e9f454c5f759f7a0a6d36ab3f63a
Volkov_P <javolkov@icloud.com>
2023-02-04 21:40:45 +0200

    update

--- for_loops.py
+++ for_loops.py
@@ -1,19 +1,19 @@
-import turtle
+# import turtle
 
-pavlo_turtle = turtle.Turtle()
-pavlo_turtle.speed(7)
-def square():
-    pavlo_turtle.forward(100)
-    pavlo_turtle.right(90)
-    pavlo_turtle.forward(100)
-    pavlo_turtle.right(90)
-    pavlo_turtle.forward(100)
-    pavlo_turtle.right(90)
-    pavlo_turtle.forward(100)
+# pavlo_turtle = turtle.Turtle()
+# pavlo_turtle.speed(7)
+# def square():
+#     pavlo_turtle.forward(100)
+#     pavlo_turtle.right(90)
+#     pavlo_turtle.forward(100)
+#     pavlo_turtle.right(90)
+#     pavlo_turtle.forward(100)
+#     pavlo_turtle.right(90)
+#     pavlo_turtle.forward(100)
 
-# pavlo = 'happy'
-# while pavlo == 'happy':
-#     pavlo_turtle.forward(1)
+# # pavlo = 'happy'
+# # while pavlo == 'happy':
+# #     pavlo_turtle.forward(1)
 
-for count in range(4):
-    square()
+# for count in range(4):
+#     square()
--- if_else_statements.py
+++ if_else_statements.py
@@ -9,18 +9,41 @@
 # else:
 #     turtle.backward(100)
 
-age = 10
-if age == 10:
-    print('go away')
+# age = 10
+# if age == 10:
+#     print('go away')
 
 
-salary = 700
-if salary >= 500 and salary <= 1000:
-    print('I\'m so happy')
+# salary = 700
+# if salary >= 500 and salary <= 1000:
+#     print('I\'m so happy')
+# else:
+#     print('don\'t worry')    
+
+# age = 38
+# converted_age = str(age)
+# print(converted_age)
+# print(type(converted_age))
+
+# money = 2000
+# if money > 1000:
+#     print('Я богат')
+# else :
+#     print('Я не богат')
+
+# twinkies = 300
+# if twinkies > 100:
+#     print('Слишком много')
+# else:
+#     print('Слишком мало')    
+
+
+ninjas = 5
+if ninjas < 50:
+    print('Их слишком много')
+elif ninjas < 30:
+    print('Будет непросто, но я с ними')
+elif ninjas < 10:
+    print('Я одолею этих ниндзя')  
 else:
-    print('don\'t worry')    
-
-age = 38
-converted_age = str(age)
-print(converted_age)
-print(type(converted_age))
+    print('Что то еще')              
